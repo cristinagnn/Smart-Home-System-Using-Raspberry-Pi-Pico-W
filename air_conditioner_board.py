e9b1f1e29cfb39ed59a5906d0b87dc59c86d4816
cristinagnn <crsgeanina@gmail.com>
2024-05-23 12:58:55 +0300

    Updated A/C and heating files

--- air_conditioner_board.py
+++ air_conditioner_board.py
@@ -1,16 +1,21 @@
 from machine import ADC, Pin
+from picozero import LED
 import utime
+
+# Initialize pins for LEDs
+green = LED(15)
+red = LED(14)
 
 # Initialize ADC for the potentiometer
 potentiometer = ADC(Pin(26))  # GP26 corresponds to ADC0
 
 # Moving average filter variables
-num_samples = 10  # Reduce the number of samples for more responsiveness
+num_samples = 10  
 adc_buffer = [0] * num_samples
 buffer_index = 0
 
 # Exponential moving average variables
-alpha = 0.3  # Increase the smoothing factor for more responsiveness
+alpha = 0.3 
 ema_value = 0
 
 def read_potentiometer():
@@ -34,17 +39,30 @@ def map_value(value, from_low, from_high, to_low, to_high):
     # Helper function to map one range of values to another
     return to_low + ((value - from_low) / (from_high - from_low)) * (to_high - to_low)
 
-while True:
-    # Read the potentiometer and get the filtered value
-    filtered_adc_value = read_potentiometer()
-    
-    # Assuming the range of the ADC is from 0 to 65535
-    min_adc = 0
-    max_adc = 65535  # Adjust this based on actual observed values
-    temperature = map_value(filtered_adc_value, min_adc, max_adc, 15, 40)
-    
-    # Print the set temperature
-    print(f"Set temperature: {temperature:.2f} °C")
-    
-    # Sleep for a short while to avoid flooding the output
-    utime.sleep_ms(100)
+def main():
+    while True:
+        # Read the potentiometer and get the filtered value
+        filtered_adc_value = read_potentiometer()
+        
+        # The range of the ADC is from 0 to 65535
+        min_adc = 0
+        max_adc = 65535  
+        temperature = map_value(filtered_adc_value, min_adc, max_adc, 15, 40)
+        
+        # Print the set temperature
+        print(f"Set temperature: {temperature:.2f} °C")
+        
+        # While temperature is higher than 26, A/C is running
+        if (temperature > 26):
+            green.on()
+            red.off()
+        else:
+            red.on()
+            green.off()
+
+        # Sleep for a short while to avoid flooding the output
+        utime.sleep_ms(100)
+
+if __name__ == "__main__":
+    main()
+
